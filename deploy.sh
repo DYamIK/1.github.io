f677042062c7c2f387674447478bdbb74ab423fc
DYamIK <3226398591@qq.com>
2026-03-02 20:55:55 +0800

    恢复标题宽度 - 2026-03-02 20:55:55

--- deploy.sh
+++ deploy.sh
@@ -25,7 +25,7 @@ git add .
 
 # 提交更改
 echo "💾 提交更改..."
-git commit -m "更新标题宽度 - $(date '+%Y-%m-%d %H:%M:%S')"
+git commit -m "恢复标题宽度 - $(date '+%Y-%m-%d %H:%M:%S')"
 
 # 推送到 GitHub
 echo "📤 推送到 GitHub..."
